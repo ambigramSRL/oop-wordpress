fd29a4100f23ad23279f5c6be956120ea3957ca8
george <george.bungarzescu@gmail.com>
2016-05-03 22:44:55 +0300

    zza

---
+++ ro.ambigram.wordpress.core/ro.ambigram.wordpress.core/WordpressConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ro.ambigram.wordpress.core
+{
+    public class WordpressConfig: ApplicationSettingsBase
+    {
+        private static WordpressConfig _single;
+
+        public WordpressConfig()
+        {
+            if (_single!=null)
+            {
+                throw new Exception("Multiple Instance not allowed");
+            }
+
+        }
+
+        public static WordpressConfig Default { get
+            {
+                if (_single == null)
+                {
+                    _single = new WordpressConfig();
+                }
+                return _single;
+            }
+            private set; }
+    }
+}
